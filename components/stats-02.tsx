f6d48a0166fb38da5d1629b61b6a39b60d6b8684
ekbrgrd <120511537+ekbrgrd@users.noreply.github.com>
2024-12-31 14:33:48 +0000

    Update stats-02.tsx

--- components/stats-02.tsx
+++ components/stats-02.tsx
@@ -6,7 +6,7 @@ export default function Stats02() {
           <div className="bg-green-600 py-3 sm:py-6 shadow-xl">
             <ul className="flex">
               <li className="relative w-1/3 px-1 text-center after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:translate-x-px after:w-0.5 after:h-16 after:bg-green-500 after:hidden sm:after:block last:after:hidden">
-                <div className="text-4xl md:text-5xl font-quicksand font-bold text-white mb-2">7</div>
+                <div className="text-4xl md:text-5xl font-quicksand font-bold text-white mb-2">27</div>
                 <div className="text-xs sm:text-sm md:text-base text-green-200 font-medium">Betalande kunder</div>
               </li>
               <li className="relative w-1/3 px-1 text-center after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:translate-x-px after:w-0.5 after:h-16 after:bg-green-500 after:hidden sm:after:block last:after:hidden">
